2f8770e40b171cfa601c88609331ae1a21e200f7
Rafael Paladini <56520474+PaMeirelles@users.noreply.github.com>
2022-05-16 22:50:53 +0000

    New prototype

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from map_generation import Map_generator
 from virus import Virus, VirusManager
 
 bg_color = (220, 230, 250)
-win = Window(600, 400, (bg_color))
+win = Window(500, 500, (bg_color))
 
 
-mg = Map_generator(0.5, 5, (0, 0, 0), win, (155, 155, 100))
-vm = VirusManager(mg.land_matrix, mg.window, 0.8, 3)
+mg = Map_generator(4, 5, (0, 0, 0), win, (155, 155, 100))
+vm = VirusManager(mg.land_matrix, mg.window, 0.9, 1, 4)
 mg.draw_land()
 running = True
 while running:  
@@ -24,6 +24,12 @@ while running:
       if e.key == pg.K_SPACE:
         for node in mg.node_list:
           vm.viruses.append(Virus(node.location[0], node.location[1], 0))
+      if e.key == pg.K_r:
+          vm.random_start(12)
+      if e.key == pg.K_c:
+        win.window.fill((bg_color))
+        mg = Map_generator(4, 5, (0, 0, 0), win, (155, 155, 100))
+        vm = VirusManager(mg.land_matrix, mg.window, 0.93, 1, 4)
   vm.main_cycle()
   pg.display.update()
 pg.quit()
